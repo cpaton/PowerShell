513e380f15d57099c92451386b86bf426fc02a72
Craig Paton <craigpaton@gmail.com>
2016-03-09 22:16:38 +0000

    Use PsGet for posh-git as it has newer version

--- Profile.ps1
+++ Profile.ps1
@@ -1,20 +1,31 @@
+if ( -not ( Get-Module PsGet -ListAvailable ) ) {
+	(new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
+}
+Import-Module PsGet
+
 if ( -not ( Get-Module posh-git -ListAvailable ) ) {
-	Install-Module -Name posh-git
+	PsGet\Install-Module -Module posh-git -DoNotPostInstall
 }
 Set-Alias ssh-add "C:\Program Files\Git\usr\bin\ssh-add.exe"
 Set-Alias ssh-agent "C:\Program Files\Git\usr\bin\ssh-agent.exe"
 
 Import-Module posh-git
 $GitPromptSettings.EnableWindowTitle = $false
+Start-SshAgent -Quiet
 
 function global:prompt() {
+	$realLASTEXITCODE = $LASTEXITCODE
+
 	Write-Host "PS $($executionContext.SessionState.Path.CurrentLocation)" -NoNewLine
 	Write-VcsStatus
+
+    $global:LASTEXITCODE = $realLASTEXITCODE
+
 	"$('>' * ($nestedPromptLevel + 1)) "
 }
 
 if ( -not ( Get-Module PSReadLine -ListAvailable ) ) {
-	Install-Module -Name PSReadLine
+	PowerShellGet\Install-Module -Name PSReadLine
 }
 
 Import-Module PSReadLine
