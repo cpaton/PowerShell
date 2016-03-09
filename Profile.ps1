83c446e6612b76a989c0b870570cc81ede711f83
Craig Paton <craigpaton@gmail.com>
2016-03-09 21:44:31 +0000

    Initial profile script

---
+++ Profile.ps1
@@ -0,0 +1,24 @@
+if ( -not ( Get-Module posh-git -ListAvailable ) ) {
+	Install-Module -Name posh-git
+}
+Set-Alias ssh-add "C:\Program Files\Git\usr\bin\ssh-add.exe"
+Set-Alias ssh-agent "C:\Program Files\Git\usr\bin\ssh-agent.exe"
+
+Import-Module posh-git
+
+function global:prompt() {
+	Write-Host "PS $($executionContext.SessionState.Path.CurrentLocation)" -NoNewLine
+	Write-VcsStatus
+	"$('>' * ($nestedPromptLevel + 1)) "
+}
+
+if ( -not ( Get-Module PSReadLine -ListAvailable ) ) {
+	Install-Module -Name PSReadLine
+}
+
+Import-Module PSReadLine
+
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Set-Location C:\_cp
