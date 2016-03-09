2a37b01860cabf5834b87e615887231f31ef5eea
Craig Paton <craigpaton@gmail.com>
2016-03-09 22:26:20 +0000

    Command to initialise docker tools

--- Profile.ps1
+++ Profile.ps1
@@ -34,3 +34,19 @@ Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 Set-Location C:\_cp
+
+function Initialize-Docker() {
+	[CmdletBinding()]
+	param()
+
+	# Make sure docker host is running
+	if ( ( docker-machine status default ) -ne "Running" ) {
+	    docker-machine start default
+	    "yes" | docker-machine regenerate-certs default
+	}
+
+	# Get machine information and setup the environment so that we can use the docker tool
+	docker-machine env default --shell powershell | Invoke-Expression
+	Write-Host ( "Docker machine IP address is {0}" -f ( docker-machine ip default ) )
+}
+Set-Alias dockerinit Initialize-Docker
